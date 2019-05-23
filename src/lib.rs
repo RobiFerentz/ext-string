c1ee9e2c6e5722442f150ce8cb34f941184e43b5
Robert Ferentz <RobiFerentz@gmail.com>
2019-05-24 00:34:49 +0300

    added pad_str functions

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,10 @@ pub trait ExtString {
     fn pad_left(&self, pad_len: usize, c: char) -> String;
 
     fn pad_right(&self, pad_len: usize, c: char) -> String;
+
+    fn pad_left_str(&self, pad_len: usize, s: &str) -> String;
+
+    fn pad_right_str(&self, pad_len: usize, s: &str) -> String;
 }
 
 impl ExtString for String {
@@ -42,6 +46,38 @@ impl ExtString for String {
         }
         pad
     }
+
+    fn pad_left_str(&self, pad_len: usize, s: &str) -> String {
+        let count = self.chars().count();
+        if pad_len <= count {
+            return self.clone();
+        }
+        
+        let repeat = pad_len - count;
+        let repeat_len = s.chars().count();
+        let mut pad = String::new();
+        for index in 0..repeat {    
+            pad.push(s.chars().nth(index % repeat_len).unwrap());
+        }
+        pad.push_str(self);
+        pad 
+    }
+    
+    fn pad_right_str(&self, pad_len: usize, s: &str) -> String {
+        let count = self.chars().count();
+        if pad_len <= count {
+            return self.clone();
+        }
+        
+        let repeat = pad_len - count;
+        let repeat_len = s.chars().count();
+        let mut pad = String::new();
+        pad.push_str(self);
+        for index in 0..repeat {    
+            pad.push(s.chars().nth(index % repeat_len).unwrap());
+        }
+        pad 
+    }
 }
 
 #[cfg(test)]
@@ -68,5 +104,21 @@ mod tests {
         let space = ' ';
         assert_eq!("12345", String::from(s).pad_right(3, space));
         assert_eq!("12345     ", String::from(s).pad_right(10, space));
+    }
+    
+    #[test]
+    fn test_pad_left_str() {
+        let s = "12345";
+        let padding = "qwerty";
+        assert_eq!("qwerty12345", String::from(s).pad_left_str(11, padding));
+        assert_eq!("qwertyqwe12345", String::from(s).pad_left_str(14, padding));
+    }
+    
+    #[test]
+    fn test_pad_right_str() {
+        let s = "12345";
+        let padding = "qwerty";
+        assert_eq!("12345qwerty", String::from(s).pad_right_str(11, padding));
+        assert_eq!("12345qwertyqwe", String::from(s).pad_right_str(14, padding));
     }
 }
